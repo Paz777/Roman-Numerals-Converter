4403afdb0fe8847bde678f240dd07352ab9e9700
Paz Sonagara <pazsonagara@Pazs-MacBook-Air.local>
2022-09-01 18:30:34 +0100

    Added all roman numerals tests and used a dictionary to hold key value pairs

--- RomanNumeralsConverter/RomanNumeralsConverter/Models/RomanNumerals.cs
+++ RomanNumeralsConverter/RomanNumeralsConverter/Models/RomanNumerals.cs
@@ -3,9 +3,23 @@ namespace RomanNumeralsConverter.Models
 {
     public class RomanNumerals
     {
+        private Dictionary<string, int> RomanNumeralList = new Dictionary<string, int>()
+        {
+            { "I",1 },
+            { "II",2 },
+            { "III",3 },
+            { "IV",4 },
+            { "V",5 },
+            { "VI",6 },
+            { "VII",7 },
+            { "VIII",8 },
+            { "IX",9 },
+            { "X",10 },
+        };
+
         public int ConvertRomanNumeral(string romanNumeral)
         {
-            return 1;
+            return RomanNumeralList[romanNumeral];
         }
     }
 }
--- RomanNumeralsConverter/RomanNumeralsConverterTests/RomanNumeralTests.cs
+++ RomanNumeralsConverter/RomanNumeralsConverterTests/RomanNumeralTests.cs
@@ -19,8 +19,16 @@ namespace RomanNumeralsConverter.Tests
         [Test]
         public void Given_A_Roman_Numeral_Should_Return_The_Correct_Number()
         {
-
             RomanNumeral.ConvertRomanNumeral("I").Should().Be(1);
+            RomanNumeral.ConvertRomanNumeral("II").Should().Be(2);
+            RomanNumeral.ConvertRomanNumeral("III").Should().Be(3);
+            RomanNumeral.ConvertRomanNumeral("IV").Should().Be(4);
+            RomanNumeral.ConvertRomanNumeral("V").Should().Be(5);
+            RomanNumeral.ConvertRomanNumeral("VI").Should().Be(6);
+            RomanNumeral.ConvertRomanNumeral("VII").Should().Be(7);
+            RomanNumeral.ConvertRomanNumeral("VIII").Should().Be(8);
+            RomanNumeral.ConvertRomanNumeral("IX").Should().Be(9);
+            RomanNumeral.ConvertRomanNumeral("X").Should().Be(10);
         }
     }
 }
